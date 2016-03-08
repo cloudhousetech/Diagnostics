7a8afb9bfe85950d715c5d2fd7124218a1172b97
dan <dan.turner@cloudhouse.com>
2016-03-08 09:27:56 +0000

    first commit

---
+++ lib/diagnostics.ex
@@ -0,0 +1,81 @@
+defmodule Diagnostics do
+  def processes_by_size(text, count), do: processes_by_size(text) |> Enum.take(count)
+  def processes_by_size(count) when is_number(count), do: processes_by_size("") |> Enum.take(count)
+  def processes_by_size(text) do
+    process_stream(text, modules_by_size)
+    |> Enum.sort_by(fn %{info: {_, size}} -> -size end)
+  end
+
+  def processes_by_large_binary_size(text, count), do: processes_by_large_binary_size(text) |> Enum.take(count)
+  def processes_by_large_binary_size(count) when is_number(count), do: processes_by_large_binary_size("") |> Enum.take(count)
+  def processes_by_large_binary_size(text) do
+    process_stream(text, modules_by_binary_refs)
+    |> Enum.sort_by(fn %{info: %{size: total}} -> -total end)
+  end
+
+  def processes_by_module do
+    process_stream(modules_by_size)
+    |> Stream.map(fn %{module: module, info: {_, size}} -> {module, size} end)
+    |> Enum.group_by(fn {module, _} -> module end)
+    |> Map.to_list
+    |> Stream.map(fn {module, list} -> {module, list |> Enum.count, list |> Enum.map(fn {_, size} -> size end) |> Enum.sum} end) 
+    |> Enum.sort_by(fn {_, _, size} -> -size end)
+  end
+
+  def module_name(pid), do: module(:erlang.process_info(pid))
+
+  def size(pid) do
+    {_, info} = :erlang.process_info(pid)
+    do_size(info)
+  end
+
+  defp process_stream("", fun), do: process_stream(fun)
+  defp process_stream(text, fun), do: process_stream(fun) |> Stream.filter(fn %{module: module} -> module |> String.contains?(text) end)
+  defp process_stream(fun) do
+    :erlang.processes
+    |> fun.()
+  end
+
+  defp modules_by_size do
+    fn modules ->
+      modules
+      |> Stream.map(fn process -> {process, :erlang.process_info(process)} end)
+      |> Stream.map(fn {process, info} -> %{module: module(info), pid: process, info: {info, do_size(info)}} end)
+    end
+  end
+
+  defp modules_by_binary_refs do
+    fn modules ->
+      modules 
+      |> Stream.map(fn process -> 
+        case :erlang.process_info(process, :binary) do
+          {:binary, bin_info} -> {process, bin_info} 
+          _ -> :no_proc
+        end
+      end)
+      |> Stream.filter(fn x -> x != :no_proc end)
+      |> Stream.map(fn {process, bin_info} -> binary_refs_count({process, bin_info}) end)
+    end
+  end
+
+  defp binary_refs_count({process, bin_info}) do
+    unique_refs = Enum.dedup_by(bin_info, fn {ref, _, _} -> ref end)
+    total_do_size = Enum.map(unique_refs, fn({_, do_size, _}) -> do_size end) |> Enum.sum
+
+    count = Enum.count bin_info
+    unique_count = Enum.count unique_refs
+
+    %{module: module(:erlang.process_info(process)), 
+      pid: process, 
+      info: %{total: count, unique: unique_count, size: total_do_size, duplicates: (count - unique_count)}}
+  end
+
+  defp do_size(:undefined), do: 0
+  defp do_size(info), do: 8 * (info[:stack_size] + info[:heap_size]) / (1024*1024)
+
+  defp module(:undefined), do: "No longer exists"
+  defp module(%{"$initial_call": {module, _, _}}), do: module |> to_string
+  defp module(%{dictionary: dictionary}), do: module dictionary
+  defp module(info) when is_list(info), do: module info |> Map.new
+  defp module(_), do: "Unknown"
+end
---
+++ test/diagnostics_test.exs
@@ -0,0 +1,53 @@
+defmodule DiagnosticsTest do
+  use ExUnit.Case
+  alias Diagnostics
+  alias Test.Process
+
+  test "largest process" do
+    {:ok, big_process} = Process.start_link Enum.to_list(1..1000)
+    {:ok, small_process} = Process.start_link :nil
+
+    [%{pid: largest_process}, %{pid: next_largest_process}] = Diagnostics.processes_by_size("Test", 2)
+    assert big_process == largest_process
+    assert small_process == next_largest_process
+
+    [%{pid: largest_process}, %{pid: next_largest_process}] = Diagnostics.processes_by_size("Test")
+    assert big_process == largest_process
+    assert small_process == next_largest_process
+
+    all_pids = Diagnostics.processes_by_size(1000)
+               |> Enum.map(fn %{pid: pid} -> pid end)
+    assert big_process in all_pids
+    assert small_process in all_pids
+  end
+
+  test "Processes by module" do
+    process_count = 10000
+    1..process_count |> Enum.each(fn _ -> Process.start_link nil end)
+    [{module_name, count, _}| _] = Diagnostics.processes_by_module
+    assert "#{Process}" == module_name
+    assert process_count == count
+  end
+
+  test "processes by large binary refs" do
+    binary1 = 1..1000 |> Enum.reduce(<<>>, fn(x, acc) -> acc <> to_string(x) end) 
+    binary2 = 1001..2000 |> Enum.reduce(<<>>, fn(x, acc) -> acc <> to_string(x) end) 
+    binary1_size = byte_size binary1
+    both_binary_size = byte_size(binary2) + byte_size(binary1)
+
+    {:ok, pid} = Process.start_link [binary1, binary2]
+    assert [%{info: %{duplicates: 0, unique: 2, total: 2, size: ^both_binary_size}}] = Diagnostics.processes_by_large_binary_size("Test", 2)
+    Agent.stop pid 
+
+    {:ok, pid} = Process.start_link [binary1, binary1]
+    assert [%{info: %{duplicates: 1, unique: 1, total: 2, size: ^binary1_size}}] = Diagnostics.processes_by_large_binary_size("Test", 1)
+    Agent.stop pid 
+  end
+
+end
+
+defmodule Test.Process do
+  def start_link(state) do
+    Agent.start_link(fn -> state end)
+  end
+end
